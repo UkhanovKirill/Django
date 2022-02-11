dcd2f0664a5a13c1b559cd273b830931863357ec
Kirill <ukhanovkirill1@gmail.com>
2022-02-11 17:42:36 +0300

    template tag 'date'

--- products/views.py
+++ products/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from datetime import datetime
 
 
 def index(request):
@@ -18,6 +19,7 @@ def products(request):
             {'name': 'Черный рюкзак Nike Heritage', 'price': '2340', 'description': 'Плотная ткань. Легкий материал.', 'image': '/static/vendor/img/products/Black-Nike-Heritage-backpack.png'},
             {'name': 'Черные туфли на платформе с 3 парами люверсов Dr Martens 1461 Bex', 'price': '13590', 'description': 'Гладкий кожаный верх. Натуральный материал.', 'image': '/static/vendor/img/products/Black-Dr-Martens-shoes.png'},
             {'name': 'Темно-синие широкие строгие брюки ASOS DESIGN', 'price': '2890', 'description': 'Легкая эластичная ткань сирсакер Фактурная ткань.', 'image': '/static/vendor/img/products/Dark-blue-wide-leg-ASOs-DESIGN-trousers.png'},
-        ]
+        ],
+        'date': datetime.now()
     }
     return render(request, 'products/products.html', context)
