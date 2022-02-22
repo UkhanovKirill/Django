b49bd7e1a851cba50572a2c98b0ef3211095755e
kirill <ukhanovkirill1@gmail.com>
2022-02-22 20:32:15 +0300

    add baskets model

--- users/views.py
+++ users/views.py
@@ -3,6 +3,7 @@ from django.contrib import auth
 from django.urls import reverse
 from django.contrib import messages
 from users.forms import UserLoginForm, UserRegistrationForm, UserProfileForm
+from baskets.models import Basket
 
 
 def login(request):
@@ -54,5 +55,9 @@ def profile(request):
             return HttpResponseRedirect(reverse('users:profile'))
     else:
         form = UserProfileForm(instance=request.user)
-    context = {'title': 'GeekShop - Профиль', 'form': form}
+    context = {
+        'title': 'GeekShop - Профиль',
+        'form': form,
+        'baskets': Basket.objects.filter(user=request.user)
+    }
     return render(request, 'users/profile.html', context)
