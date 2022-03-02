4ede0c1fc9b28a6bef50a52ca8db4f998ca643bb
kirill <ukhanovkirill1@gmail.com>
2022-03-02 17:15:27 +0300

    add date

--- admins/views.py
+++ admins/views.py
@@ -1,18 +1,22 @@
 from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
+from datetime import datetime
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
+date = datetime.now()
+
+
 def index(request):
-    context = {'title': 'GeekShop - Admin'}
+    context = {'title': 'GeekShop - Admin', 'date': date}
     return render(request, 'admins/admin.html', context)
 
 
 def admin_users(request):
     users = User.objects.all()
-    context = {'title': 'GeekShop - Admin', 'users': users}
+    context = {'title': 'GeekShop - Admin', 'users': users, 'date': date}
     return render(request, 'admins/admin-users-read.html', context)
 
 
@@ -25,7 +29,7 @@ def admin_users_create(request):
             return HttpResponseRedirect(reverse('admin_staff:admin_users'))
     else:
         form = UserAdminRegistrationForm()
-    context = {'title': 'GeekShop - Admin', 'form': form}
+    context = {'title': 'GeekShop - Admin', 'form': form, 'date': date}
     return render(request, 'admins/admin-users-create.html', context)
 
 
@@ -39,7 +43,7 @@ def admin_users_update(request, pk):
             return HttpResponseRedirect(reverse('admin_staff:admin_users'))
     else:
         form = UserAdminProfileForm(instance=select_user)
-    context = {'title': 'GeekShop - Admin', 'form': form, 'select_user': select_user}
+    context = {'title': 'GeekShop - Admin', 'form': form, 'select_user': select_user, 'date': date}
     return render(request, 'admins/admin-users-update-delete.html', context)
 
 
