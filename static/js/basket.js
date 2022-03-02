35dcdef1662b3f4411f3ee6fbf9ebd510acd4588
kirill <ukhanovkirill1@gmail.com>
2022-03-02 12:39:44 +0300

    ajax in product (basket_add)

--- static/js/basket.js
+++ static/js/basket.js
@@ -8,4 +8,14 @@ window.onload = function () {
             }
         })
     })
+
+    $('.product_list').on('click', 'button[type="button"]', function () {
+        let target = event.target.value;
+        $.ajax({
+            url: '/baskets/basket-add/' + target + '/',
+            success: function (data) {
+                $('.product_list').html(data.result);
+            }
+        })
+    })
 }
