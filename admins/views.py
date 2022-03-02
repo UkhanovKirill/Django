765ae68fe3fbbb8521601ddec73688cdab51a896
kirill <ukhanovkirill1@gmail.com>
2022-03-02 16:54:11 +0300

    add admin_users_update()

--- admins/views.py
+++ admins/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, HttpResponseRedirect
 from django.urls import reverse
 from django.contrib import messages
 from users.models import User
-from admins.forms import UserAdminRegistrationForm
+from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 
 
 def index(request):
@@ -29,4 +29,15 @@ def admin_users_create(request):
     return render(request, 'admins/admin-users-create.html', context)
 
 
-
+def admin_users_update(request, pk):
+    select_user = User.objects.get(id=pk)
+    if request.method == 'POST':
+        form = UserAdminProfileForm(instance=select_user, data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Изменения внесены!')
+            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+    else:
+        form = UserAdminProfileForm(instance=select_user)
+    context = {'title': 'GeekShop - Admin', 'form': form, 'select_user': select_user}
+    return render(request, 'admins/admin-users-update-delete.html', context)
