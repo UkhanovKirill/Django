88c73a97b0230d2a752d2023528ea98d69df64a9
kirill <ukhanovkirill1@gmail.com>
2022-03-02 16:24:27 +0300

    add admin_users_create() and admin_users_read()

--- admins/views.py
+++ admins/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib import messages
 from users.models import User
+from admins.forms import UserAdminRegistrationForm
 
 
 def index(request):
@@ -11,4 +14,19 @@ def admin_users(request):
     users = User.objects.all()
     context = {'title': 'GeekShop - Admin', 'users': users}
     return render(request, 'admins/admin-users-read.html', context)
+
+
+def admin_users_create(request):
+    if request.method == 'POST':
+        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Пользователь создан!')
+            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+    else:
+        form = UserAdminRegistrationForm()
+    context = {'title': 'GeekShop - Admin', 'form': form}
+    return render(request, 'admins/admin-users-create.html', context)
+
+
 
