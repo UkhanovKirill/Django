8957e94b0bffaa363356a7a1572c8fb5bc43a25d
kirill <ukhanovkirill1@gmail.com>
2022-03-14 17:35:23 +0300

    change control UserAdminListView, UserAdminCreateView, UserAdminUpdateView, UserAdminDeleteView

--- admins/views.py
+++ admins/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render, HttpResponseRedirect
-from django.urls import reverse
-from django.contrib import messages
+from django.urls import reverse_lazy
+from django.contrib.messages.views import SuccessMessageMixin
 from datetime import datetime
+from django.utils.decorators import method_decorator
 from django.contrib.auth.decorators import user_passes_test
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from users.models import User
 from admins.forms import UserAdminRegistrationForm, UserAdminProfileForm
 from products.models import Product, ProductCategory
@@ -17,48 +20,58 @@ def index(request):
     return render(request, 'admins/admin.html', context)
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users(request):
-    users = User.objects.all()
-    context = {'title': 'GeekShop - Admin', 'users': users, 'date': date}
-    return render(request, 'admins/admin-users-read.html', context)
+class CommonMixin(SuccessMessageMixin):
+    title = None
+    date = datetime.now()
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super(CommonMixin, self).get_context_data(object_list=None, **kwargs)
+        context['title'] = self.title
+        context['date'] = self.date
+        return context
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_create(request):
-    if request.method == 'POST':
-        form = UserAdminRegistrationForm(data=request.POST, files=request.FILES)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Пользователь создан!')
-            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
-    else:
-        form = UserAdminRegistrationForm()
-    context = {'title': 'GeekShop - Admin', 'form': form, 'date': date}
-    return render(request, 'admins/admin-users-create.html', context)
+class UserAdminListView(CommonMixin, ListView):
+    model = User
+    template_name = 'admins/admin-users-read.html'
+    title = 'GeekShop - Админка'
+
+    @method_decorator(user_passes_test(lambda u: u.is_staff))
+    def dispatch(self, request, *args, **kwargs):
+        return super(UserAdminListView, self).dispatch(request, *args, **kwargs)
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_update(request, pk):
-    select_user = User.objects.get(id=pk)
-    if request.method == 'POST':
-        form = UserAdminProfileForm(instance=select_user, data=request.POST)
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Изменения внесены!')
-            return HttpResponseRedirect(reverse('admin_staff:admin_users'))
-    else:
-        form = UserAdminProfileForm(instance=select_user)
-    context = {'title': 'GeekShop - Admin', 'form': form, 'select_user': select_user, 'date': date}
-    return render(request, 'admins/admin-users-update-delete.html', context)
+class UserAdminCreateView(CommonMixin, CreateView):
+    model = User
+    form_class = UserAdminRegistrationForm
+    template_name = 'admins/admin-users-create.html'
+    success_url = reverse_lazy('admin_staff:admin_users')
+    success_message = 'Пользователь создан!'
+    title = 'GeekShop - Админка'
 
 
-@user_passes_test(lambda u: u.is_staff)
-def admin_users_delete(request, pk):
-    user = User.objects.get(id=pk)
-    user.is_active = False
-    user.save()
-    return HttpResponseRedirect(reverse('admin_staff:admin_users'))
+class UserAdminUpdateView(CommonMixin, UpdateView):
+    model = User
+    form_class = UserAdminProfileForm
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admin_staff:admin_users')
+    success_message = 'Изменения внесены!'
+    title = 'GeekShop - Админка'
+
+
+class UserAdminDeleteView(CommonMixin, DeleteView):
+    model = User
+    template_name = 'admins/admin-users-update-delete.html'
+    success_url = reverse_lazy('admin_staff:admin_users')
+    title = 'GeekShop - Админка'
+
+    def delete(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        self.object.safe_delete()
+        return HttpResponseRedirect(self.success_url)
+
+
+
 
 
 @user_passes_test(lambda u: u.is_staff)
