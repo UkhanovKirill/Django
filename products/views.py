2f8a47cb55b1835e1dd355642cb37e1df010a9fe
kirill <ukhanovkirill1@gmail.com>
2022-03-14 15:33:44 +0300

    add categories and paginator page

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ import json
 from django.shortcuts import render
 from datetime import datetime
 from django.conf import settings
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 from products.models import Product, ProductCategory
 
@@ -13,13 +14,23 @@ def index(request):
     return render(request, 'products/index.html', context)
 
 
-def products(request):
-    file_path = settings.BASE_DIR / 'products/fixtures/goods.json'
+def products(request, category_id=None, page=1):
+    if category_id:
+        products = Product.objects.filter(category_id=category_id)
+    else:
+        products = Product.objects.all()
+
+    paginator = Paginator(products, 3)
+    try:
+        products_paginator = paginator.page(page)
+    except PageNotAnInteger:
+        products_paginator = paginator.page(1)
+    except EmptyPage:
+        products_paginator = paginator.page(paginator.num_pages)
     context = {
         'title': 'Geeksop - Каталог',
-        'products': Product.objects.all(),
         'categories': ProductCategory.objects.all(),
-        #'products': json.load(open(file_path, encoding='utf-8')),
+        'products': products_paginator,
         'date': datetime.now()
     }
     return render(request, 'products/products.html', context)
