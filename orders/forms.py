993035497faf8031fb6f5130c5cf647d680182ba
kirill <ukhanovkirill1@gmail.com>
2022-04-15 00:00:04 +0300

    templates and views in orders

--- orders/forms.py
+++ orders/forms.py
@@ -1,16 +1,29 @@
 from django import forms
-from users.forms import UserRegistrationForm, UserProfileForm
-from users.models import User
+from orders.models import Order, OrderItem
 
 
-class UserAdminRegistrationForm(UserRegistrationForm):
-    image = forms.ImageField(widget=forms.FileInput(attrs={'class': 'custom-file-input'}), required=False)
+class OrderForm(forms.ModelForm):
 
     class Meta:
-        model = User
-        fields = ('first_name', 'last_name', 'username', 'email', 'password1', 'password2', 'image')
+        model = Order
+        exclude = ('user',)
+
+    def __init__(self, *args, **kwargs):
+        super(OrderForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
 
 
-class UserAdminProfileForm(UserProfileForm):
-    username = forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control py-4'}))
-    email = forms.CharField(widget=forms.EmailInput(attrs={'class': 'form-control py-4'}))
+class OrderItemForm(forms.ModelForm):
+    price = forms.CharField(label='цена', required=False)
+
+    class Meta:
+        model = OrderItem
+        fields = '__all__'
+
+    def __init__(self, *args, **kwargs):
+        super(OrderItemForm, self).__init__(*args, **kwargs)
+
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control'
--- orders/urls.py
+++ orders/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
+from orders.views import OrderListView, OrderCreateView, OrderDeleteView, OrderUpdateView, OrderDetailView, order_forming_complete
 
-app_name = 'admins'
+app_name = 'orders'
 
 urlpatterns = [
-    # path('', , name='index'),
+    path('', OrderListView.as_view(), name='list'),
+    path('create/', OrderCreateView.as_view(), name='create'),
+    path('update/<int:pk>/', OrderUpdateView.as_view(), name='update'),
+    path('read/<int:pk>/', OrderDetailView.as_view(), name='read'),
+    path('delete/<int:pk>/', OrderDeleteView.as_view(), name='delete'),
+    path('order_complete/<int:pk>/', order_forming_complete, name='order_complete'),
     ]
